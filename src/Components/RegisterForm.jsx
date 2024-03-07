6bb044218f5e7394581aa2537982381531fe61a0
Patrick Nguyen <patar.nguyen@gmail.com>
2024-03-06 22:37:32 -0500

    update post request

--- src/Components/RegisterForm.jsx
+++ src/Components/RegisterForm.jsx
@@ -2,30 +2,46 @@ import React, {useState} from 'react'
 import { NavLink } from 'react-router-dom'
 import './Assets/RegisterForm.css'
 import { FaUser, FaLock } from "react-icons/fa";
+import axios from 'axios';
 
 export const RegisterForm = () => {
   
   const provinces = ["NL", "PE", "NS", "NB", "QC", "ON", "MB", "SK", "AB", "BC", "YT", "NT", "NU"];
 
-  const [username, setUsername] = useState('');
+  const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [birthdate, setBirthdate] = useState('');
-  const [address, setAddress] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('');
+  const [street, setStreet] = useState('');
   const [city, setCity] = useState('');
   const [province, setProvince] = useState('');
-  const [zip, setZip] = useState('');
+  const [zipcode, setZipcode] = useState('');
   const [country, setCountry] = useState('');
 
 
   const onSubmitForm = (e) => {
     e.preventDefault();
-    // axios.post("", {username, password, email, firstName, lastName, birthdate, address, city, province, zip, country})
-    // .then(res => console.log(res))
-    // .catch(err => console.log(err))
-    console.log(`${firstName} ${lastName} is my name`)
+    var userAccount = {
+      "userName": userName,
+      "password": password,
+      "email": email,
+      "firstName": firstName,
+      "lastName": lastName,
+      "dateOfBirth": dateOfBirth,
+      "street": street,
+      "city": city,
+      "province": province,
+      "zipcode": zipcode,
+      "country": country
+    }
+    console.log(JSON.stringify(userAccount));
+    axios.post("http://99.234.6.118:50023/UserAccount", 
+    JSON.stringify(userAccount), 
+    {headers: { "Content-Type": "application/json"}})
+    .then(res => console.log(res))
+    .catch(err => console.log(err.response.data))
   }
 
   return (
@@ -34,7 +50,7 @@ export const RegisterForm = () => {
         <h1>Register</h1>
 
         <div className="input-box2">
-          <input type="text" placeholder="Username" value={username} onChange={e => {setUsername(e.target.value)}}required />
+          <input type="text" placeholder="Username" value={userName} onChange={e => {setUserName(e.target.value)}}required />
           <FaUser className="icon"/>
         </div>
 
@@ -59,12 +75,12 @@ export const RegisterForm = () => {
 
 
         <div className="input-box2">
-          <input type="text" placeholder="Date of Birth" value={birthdate} onChange={e => {setBirthdate(e.target.value)}} required />
+          <input type="text" placeholder="Date of Birth" value={dateOfBirth} onChange={e => {setDateOfBirth(e.target.value)}} required />
         </div>
 
 
         <div className="input-box2">
-          <input type="text" placeholder="Street Address" value={address} onChange={e => {setAddress(e.target.value)}}/>
+          <input type="text" placeholder="Street Address" value={street} onChange={e => {setStreet(e.target.value)}}/>
         </div>
 
         <div className="input-box2">
@@ -81,7 +97,7 @@ export const RegisterForm = () => {
         </div>
 
         <div className="input-box2">
-          <input type="text" placeholder="Zip Code" value={zip} onChange={e => {setZip(e.target.value)}} required />
+          <input type="text" placeholder="Zip Code" value={zipcode} onChange={e => {setZipcode(e.target.value)}} required />
         </div>
 
         <div className="input-box2">
