dfc13d0b8f392c68bb26acd98b14f6ab4d498141
Patrick Nguyen <patar.nguyen@gmail.com>
2024-03-18 12:48:09 -0400

    add hamburger menu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import { NavLink, Link } from 'react-router-dom';
 import './Assets/Navbar.css'
 
 export const Navbar = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const showSidebar = (e) => {
+    e.preventDefault();
+    setSidebarOpen(true);
+  }
+
+  const hideSidebar = () => {
+    setSidebarOpen(false);
+  }
+
   return (
-    <nav className="navbar">
+    <nav>
+        <ul>
+          <li className="logo"><Link to="/home">B & P Auto</Link></li>
+          <li className="hideOnMobile"><NavLink to="/login">Login</NavLink></li>
+          <li className="hideOnMobile"><NavLink to="/register">Register</NavLink></li>
+          <li className="menu-button" onClick={showSidebar}><a href=""><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
+        </ul>
 
-        <Link to="/home" className="logo">B & P Auto</Link>
-
-        <ul className="nav-links">
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
+        <ul className={sidebarOpen ? 'sidebar open' : 'sidebar'}>
+          <li onClick={hideSidebar}><a href=""><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
+          <li><NavLink to="/login">Login</NavLink></li>
+          <li><NavLink to="/register">Register</NavLink></li>
         </ul>
 
     </nav>
