665a87953e6984654452fb7e86dc302f4503a341
Patrick Nguyen <patar.nguyen@gmail.com>
2024-03-20 19:41:21 -0400

    update sidebar menu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -23,7 +23,7 @@ export const Navbar = () => {
           <li className="menu-button" onClick={showSidebar}><a href=""><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
         </ul>
 
-        <ul className={sidebarOpen ? 'sidebar open' : 'sidebar'}>
+        <ul className={sidebarOpen ? "sidebar open" : "sidebar"}>
           <li onClick={hideSidebar}><a href=""><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
           <li><NavLink to="/login">Login</NavLink></li>
           <li><NavLink to="/register">Register</NavLink></li>
